aa2b83dc99e5370d815c94be46576aa51921fd5c
Jason Tham <jason.tham@tempus.com>
2016-11-22 13:43:28 -0600

    added all data and seed script

---
+++ data/seed.sh
@@ -0,0 +1,3 @@
+mongoimport --db HAB2 --collection master --type csv --file Master.csv --headerline
+mongoimport --db HAB2 --collection batting --type csv --file Batting.csv --headerline
+mongoimport --db HAB2 --collection pitching --type csv --file Pitching.csv --headerline
