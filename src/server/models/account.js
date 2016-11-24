3daf4511ea4b777e7f113a7f9f19d4c803a258e5
Jason Tham <jtham@sbcglobal.net>
2016-11-23 21:10:05 -0600

    registration working with bcrypt and mongoose

--- src/server/models/account.js
+++ src/server/models/account.js
@@ -10,22 +10,42 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
+    password: {
+        type: String,
+        required:true
+    },
     email: {
         type: String,
         required: true,
         unique: true
     },
-    hash: String,
-    salt: String
 });
 
-userSchema.methods.setPassword = function(password){
+userSchema.pre('save', function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
     bcrypt.genSalt(saltRounds, function(err, salt) {
-        bcrypt.hash(passwords, salt, function(err, hash) {
-            // Store hash in your password DB.
-            this.hash = hash;
-            this.salt = salt;
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
         });
+    });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
     });
 };
 
--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -3,6 +3,7 @@
  */
 
 import { Router } from 'express'
+import account from '../models/account';
 
 const router = Router()
 
@@ -13,5 +14,17 @@ router.get('/test', (req, res) => {
 router.get('/tester', (req, res) => {
     res.json({test: 'test', what: 'what'});
 })
+
+router.post('/register', (req, res) => {
+    console.log("Req.body: ", req.body);
+    account.create({username: req.body.username, email: req.body.email, password:req.body.password}, (err, user) => {
+        console.log(err)
+        console.log(user)
+        if (err) {
+            res.sendStatus(400).send();
+        }
+        res.json({message: "Created User!", username: user.username});
+    });
+});
 
 export default router;
