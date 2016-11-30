40439c5d958692349855bb04a26f5fa78b2eb544
Jason Tham <jason.tham@tempus.com>
2016-11-30 11:28:01 -0600

    added saga and authorization functions

--- src/client/sagas/index.js
+++ src/client/sagas/index.js
@@ -1,5 +1,5 @@
 import { takeEvery, delay } from 'redux-saga';
-import { put, call } from 'redux-saga/effects';
+import { put, call, fork, race, take } from 'redux-saga/effects';
 
 export function* helloSaga() {
   console.log('Hello Sagas!')
@@ -21,4 +21,35 @@ export default function* rootSaga() {
     helloSaga(),
     watchIncrementAsync()
   ]
+}
+
+export function * authorize ({username, password, isRegistering}) {
+  // We send an action that tells Redux we're sending a request
+  yield put({type: SENDING_REQUEST, sending: true})
+
+  // We then try to register or log in the user, depending on the request
+  try {
+    let response
+
+    // For either log in or registering, we call the proper function in the `auth`
+    // module, which is asynchronous. Because we're using generators, we can work
+    // as if it's synchronous because we pause execution until the call is done
+    // with `yield`!
+    if (isRegistering) {
+      response = yield call(auth.register, username, password);
+    } else {
+      response = yield call(auth.login, username, password);
+    }
+
+    return response
+  } catch (error) {
+    console.log('hi')
+    // If we get an error we send Redux the appropiate action and return
+    yield put({type: REQUEST_ERROR, error: error.message})
+
+    return false
+  } finally {
+    // When done, we tell Redux we're not in the middle of a request any more
+    yield put({type: SENDING_REQUEST, sending: false})
+  }
 }
