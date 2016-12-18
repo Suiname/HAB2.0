3222c9189c7794256975bc1dfd3c937bc77e00ee
Jason Tham <jtham@sbcglobal.net>
2016-12-18 03:52:36 -0600

    name field updating

--- src/client/components/League/Create.jsx
+++ src/client/components/League/Create.jsx
@@ -4,18 +4,35 @@ import { bindActionCreators } from 'redux';
 import { createLeague } from '../../actions';
 
 class CreateComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.update = this.update.bind(this);
+    this.updateLeague = this.updateLeague.bind(this);
+    this.state = { leagueName: '', team1: '', maxPlayers: 0 };
+  }
+  update(value, key, that) {
+    console.log("value: ", value);
+    console.log("Key: ", key);
+    that.setState((state) => {
+      state[key] = value;
+    });
+  }
+  updateLeague(event) {
+    event.preventDefault();
+    console.log(event.target.value);
+    const value = event.target.value;
+    this.update(value, 'leagueName', this);
+  }
   render() {
-    if (!this.props) {
-      return null;
-    }
-    console.log("Props: ", this.props);
+    console.log("Props in Create  : ", this.props);
+    console.log("this.props.")
     return (
       <div id="create_wrapper">
         <div className="header create">Create a new league and invite friends!</div>
         <div id="createLeague" className="inline" action="/league/create" method="post">
-          <input id="league_name" className="formInput" value={this.props.leagueName} type="text" name="league_name" placeholder="League Name" />
-          <input id="owner_one" className="formInput" type="text" value={this.props.team1} name="team1_owner" placeholder="Team 1 Owner" />
-          <input id="max_players" className="formInput" type="text" name="max_players" value={this.props.maxPlayers} placeholder="Max Players" />
+          <input id="league_name" className="formInput" value={this.state.leagueName} onChange={this.updateLeague} type="text" name="league_name" placeholder="League Name" />
+          <input id="owner_one" className="formInput" type="text" value={this.state.team1} name="team1_owner" placeholder="Team 1 Owner" />
+          <input id="max_players" className="formInput" type="text" name="max_players" value={this.state.maxPlayers} placeholder="Max Players" />
           <button className="button-primary formInput" type="button">submit</button>
         </div>
       </div>
@@ -30,10 +47,11 @@ CreateComponent.propTypes = {
 };
 
 function mapStateToProps(state) {
+  console.log("State: ", state);
   return {
-    leagueName: state.leagueState.leagueName || '',
-    team1: state.leagueState.team1 || '',
-    maxPlayers: state.leagueState.maxPlayers || 0,
+    leagueName: state.leagueState.leagueName,
+    team1: state.leagueState.team1,
+    maxPlayers: state.leagueState.maxPlayers,
   };
 }
 
