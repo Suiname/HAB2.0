1a67f30fd2390020b697078f8922c650910f8f7a
Jason Tham <jtham@sbcglobal.net>
2016-12-18 21:31:22 -0600

    component submitting to state

--- src/client/components/League/Create.jsx
+++ src/client/components/League/Create.jsx
@@ -1,27 +1,30 @@
 import React, { PropTypes } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { createLeague } from '../../actions';
 
 class CreateComponent extends React.Component {
   constructor(props) {
     super(props);
     this.update = this.update.bind(this);
     this.updateLeague = this.updateLeague.bind(this);
+    this.submit = this.submit.bind(this);
+    this.createLeague = this.props.createLeague;
     this.state = { leagueName: '', team1: '', maxPlayers: 0 };
   }
-  update(value, key, that) {
+  update(value, key) {
     console.log("value: ", value);
     console.log("Key: ", key);
-    that.setState((state) => {
+    this.setState((state) => {
       state[key] = value;
     });
+  }
+  submit() {
+    console.log("State on submit: ", this.state);
+    this.createLeague(this.state);
   }
   updateLeague(event) {
     event.preventDefault();
     console.log(event.target.value);
     const value = event.target.value;
-    this.update(value, 'leagueName', this);
+    this.update(value, 'leagueName');
   }
   render() {
     console.log("Props in Create  : ", this.props);
@@ -33,7 +36,7 @@ class CreateComponent extends React.Component {
           <input id="league_name" className="formInput" value={this.state.leagueName} onChange={this.updateLeague} type="text" name="league_name" placeholder="League Name" />
           <input id="owner_one" className="formInput" type="text" value={this.state.team1} name="team1_owner" placeholder="Team 1 Owner" />
           <input id="max_players" className="formInput" type="text" name="max_players" value={this.state.maxPlayers} placeholder="Max Players" />
-          <button className="button-primary formInput" type="button">submit</button>
+          <button className="button-primary formInput" onClick={this.submit} type="button">submit</button>
         </div>
       </div>
     );
@@ -41,22 +44,7 @@ class CreateComponent extends React.Component {
 }
 
 CreateComponent.propTypes = {
-  leagueName: PropTypes.string,
-  team1: PropTypes.string,
-  maxPlayers: PropTypes.number,
+  createLeague: PropTypes.func,
 };
 
-function mapStateToProps(state) {
-  console.log("State: ", state);
-  return {
-    leagueName: state.leagueState.leagueName,
-    team1: state.leagueState.team1,
-    maxPlayers: state.leagueState.maxPlayers,
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ createLeague }, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CreateComponent);
+export default CreateComponent;
